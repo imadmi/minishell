038e3b871139ab4a4765ffee91137a8b8615a537
Imad Mimouni <imad.mimouni.123@gmail.com>
2023-03-09 08:40:35 +0100

    update

--- parsing.h
+++ parsing.h
@@ -1,24 +1,10 @@
 #ifndef PARSING_H
 #define PARSING_H
 # include <stdio.h>
-# include <errno.h>
-# include <signal.h>
-# include <fcntl.h>
-# include <limits.h>
+# include <unistd.h>
+#include "./libft/libft.h"
 # include <readline/readline.h>
 # include <readline/history.h>
-# include <sys/stat.h>
-# include <termios.h>
-# include <unistd.h>
-# include <stdlib.h>
-#include "./libft/libft.h"
-
-typedef struct s_exe
-{
-	int	b_fail_malloc;
-	int	b_pipe;
-	int	b_parssing;
-}	t_exe;
 
 #define S_QUOTE 0
 #define D_QUOTE 1
@@ -29,13 +15,18 @@ typedef struct s_exe
 #define PIPE 6
 #define WORD 7
 
+typedef struct		s_exe
+{
+	int		b_fail_malloc;
+	int				b_pipe;
+	int			b_parssing;
+}					t_exe;
+
 typedef struct s_token
 {
 	int				type;
 	char			*value;
 	struct s_token	*next;
 }					t_token;
-
-
 
 #endif 
